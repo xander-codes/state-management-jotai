9af17f4df0aa3b57948dbfdaf878add9eb21f8ab
Alex Anghel <alexisky@gmail.com>
2021-09-04 00:52:45 +0300

    test

--- src/STATES.js
+++ src/STATES.js
@@ -1,7 +1,7 @@
 import {atom} from "jotai";
 
 //initial country
-export const COUNTRY_ATOM = atom('Spain');
+export const COUNTRY_ATOM = atom('Germany');
 //initial cities array
 export const CITIES_ATOM = atom(['Tokyo', 'Osaka']);
 
